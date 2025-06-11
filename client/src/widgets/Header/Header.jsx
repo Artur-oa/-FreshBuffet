e1c544e316ebf60ad08b115e6843b38deaa399e4
medvtanya <medvtanya13@gmail.com>
2025-06-11 22:34:58 +0300

    ClientBag

--- client/src/widgets/Header/Header.jsx
+++ client/src/widgets/Header/Header.jsx
@@ -1,7 +1,56 @@
+import React from "react"
+import { NavLink, useNavigate } from "react-router"
+import UserApi from "../../entities/user/UserApi"
+
 function Header({ user, setUser }) {
+
+const navigate = useNavigate()
+
+  const logoutHandler = async () => {
+    try {
+      const data = await UserApi.logout()
+      // console.log(data)
+      if (data.statusCode === 200) {
+        setUser(() => ({}))
+        navigate('/')
+      } else {
+        console.log(data.error)
+      }
+    } catch (error) {
+      console.log(error)
+      return alert(error.response.data.error)
+    }
+  }
+
   return (
     <header className='p-4 bg-gray-100 text-center'>
       <h1 className='text-xl font-semibold'>🍽️ Fresh Buffet</h1>
+
+
+    
+      <div className="max-w-700 center wrap-float">
+        <nav className="clearfix mar-b-1">
+          <ul className="no-bullets no-margin no-padding right">
+            <li className="pipe-separate t-light-green left">
+              <NavLink to="/">Главная страница</NavLink>
+            </li>
+            {user.name ? (
+              <li className="pipe-separate t-light-green left" onClick={logoutHandler}>
+                <NavLink to="/">Выйти</NavLink>
+              </li>
+            ) : (
+              <>
+                <li className="pipe-separate t-light-green left">
+                  <NavLink to="/auth">Войти</NavLink>
+                </li>
+                <li className="pipe-separate t-light-green left" >
+                  <NavLink to="/auth">Регистрация</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
