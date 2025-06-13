97debc908700c382e1c022d93c748234489d6ec4
Artur-oa <arturogan@yandex.ru>
2025-06-13 12:06:39 +0300

    add iconStar in RecipeDetailsPage

--- client/src/pages/RecipeDetailsPage.jsx
+++ client/src/pages/RecipeDetailsPage.jsx
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import RecipesApi from '../entities/recipes/RecipesApi';
+import IconStar from '../shared/ui/FavoriteIcon/IconStar';
+import UserApi from '../entities/user/UserApi';
 
-export default function RecipeDetailsPage({ user }) {
+
+export default function RecipeDetailsPage({ user, setUser }) {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  async function handleFavorite(recipeId, userId) {
+     try {
+       if (!user) {
+         alert(
+           "Добавление в избранное доступно только для зарегистрированных пользователей!"
+         );
+         return;
+       }
+ 
+       if (!user.favorites) {
+         user.favorites = [];
+       }
+ 
+       let response;
+       if (user.favorites.includes(recipeId)) {
+         response = await UserApi.removeFavorite(recipeId, userId);
+       } else {
+         response = await UserApi.addFavorite(recipeId, userId);
+       }
+ 
+       if (response.statusCode === 200 || response.statusCode === 201) {
+         // Обновляем список избранного у пользователя только после успешного ответа от сервера
+         const updatedUser = { ...user };
+         if (user.favorites.includes(recipeId)) {
+           updatedUser.favorites = updatedUser.favorites.filter(
+             id => id !== recipeId
+           );
+         } else {
+           updatedUser.favorites = [...updatedUser.favorites, recipeId];
+         }
+         setUser(updatedUser);
+       } else {
+         throw new Error(response.message || "Ошибка при обновлении избранного");
+       }
+     } catch (err) {
+       alert("Ошибка при обновлении избранного!");
+       console.error(err);
+     }
+   }
 
   useEffect(() => {
     const fetchRecipe = async () => {
@@ -29,7 +72,7 @@ export default function RecipeDetailsPage({ user }) {
   if (!recipe) return <div>Рецепт не найден</div>;
 
   return (
-    <div className='max-w-3xl mx-auto bg-white rounded-2xl shadow-md border border-orange-100 pb-8 pt-8 pl-10 pr-10 mt-2 mb-5'>
+    <div className='max-w-3xl mx-auto bg-white rounded-2xl shadow-md border border-orange-100 pb-8 pt-8 pl-10 pr-10 mt-2 mb-5 relative'>
       <h1 className='text-3xl font-bold text-center text-orange-600 mb-6 tracking-tight'>
         {recipe.title}
       </h1>
@@ -64,6 +107,20 @@ export default function RecipeDetailsPage({ user }) {
           {recipe.instructions}
         </p>
       </div>
+      <IconStar
+        isFavorite={user?.favorites?.includes(recipe.id) || false}
+        onClick={(e) => {
+          e.stopPropagation();
+          if (!user) {
+            alert(
+              'Добавление в избранное доступно только для зарегистрированных пользователей!'
+            );
+            return;
+          }
+          handleFavorite(recipe.id, user.id);
+        }}
+        className="absolute bottom-2 right-3 z-10 transition-transform group-hover:scale-108 group-hover:text-orange-400 cursor-pointer"
+      />
     </div>
   );
 }
